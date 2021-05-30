4426a9bb31dcea15022ffbb987017b05c78e8685
15719062 <>
2021-05-30 17:06:53 +0000

    I added the np "x" ,"y" to make random numbers.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -1,61 +1,68 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Problem 1: Simple scatter plot using random 
-# 
+# ## Problem 1: Simple scatter plot using random
+#
 # We can generate random numbers using using a method `random.rand()` from the [NumPy package](https://numpy.org/). This example generates 10 random values:
-# 
+#
 # ```
 # import numpy as np
 # random_numbers = np.random.rand(10)
-# 
+#
 # ```
-# 
+#
 # ### Part 1
-# 
+#
 # Create an new data frame called `data` and add 1000 random numbers (`float`) into a new column `x` and another 1000 random numbers (`float`) into a new column `y`.
 
 import numpy as np
 import pandas as pd
 
 # YOUR CODE HERE 1 to set data
-
+x = np.random.rand(1000)
+y = np.random.rand(1000)
+data = pd.DataFrame()
+data["x"] = x
+data["y"] = y
 # Check your random values
 print(data.head())
 
 # Check that you have the correct number of rows
 assert len(data) == 1000, "There should be 1000 rows of data."
 
-
 # ### Part 2
-# 
+#
 
 # YOUR CODE HERE 2 to set colors
-
+colors = np.random.rand(1000)
 # This test print should print out 10 first numbers in the variable colors
 print(colors[0:10])
 
 # Check that the length matches
 assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 
-
-# ### Part 3 
-# 
+# ### Part 3
+#
 # #### Part 3.1
-# 
+#
 # Create a scatter plot of points with random colors
-# 
+#
 # #### Part 3.2
-# 
+#
 # #### Part 3.3
-# 
+#
 
 # Plot a scatter plot
 # YOUR CODE HERE 3
+import matplotlib.pyplot as plt
 
+plt.scatter(x, y, s=50, c=colors,cmap='rainbow' ,edgecolor='black')
 # Add labels and title
 # YOUR CODE HERE 4
-
+plt.title("rainbow plot")
+plt.xlabel("xLabel")
+plt.ylabel("yLabel")
+plt.show()
 # Save the plot as a png file:
 outputfp = "my_first_plot.png"
 
@@ -69,9 +76,8 @@ import os
 
 assert os.path.exists(outputfp), "Can't find the output image."
 
-
 # Remember to commit your changes (including the image file) to your GitHub repo!
-# 
+#
 # ### Done!
-# 
+#
 # Now you can move to [problem 2](Exercise-7-problem-2.ipynb).
