fac2d88782e26b79385297525ddf0424f5f1eaea
15719062 <>
2021-05-30 17:36:41 +0000

    I added the data to read data.

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -20,6 +20,7 @@
 import pandas as pd
 fp="data/helsinki-vantaa.csv"
 data = pd.read_csv(fp,parse_dates=['DATE'],index_col='DATE')
+
 # This test print should print first five rows
 print(data.head())
 
